8b08d41871b3531992a7fe2116e456af88c40d6c
Gabriel Tirso <gabriel.tirso@gorillasystem.com.br>
2026-02-21 01:48:26 +0000

    feat: implement session deletion

--- models/session.js
+++ models/session.js
@@ -27,7 +27,7 @@ async function findOneValidByToken(sessionToken) {
 
     if (results.rowCount === 0) {
       throw new UnauthorizedError({
-        message: "Usuário não possui sess~ao ativa.",
+        message: "Usuário não possui sessão ativa.",
         action: "Verifique se este usuário está logado e tente novamente.",
       });
     }
@@ -86,10 +86,35 @@ async function renew(sessionId) {
   }
 }
 
+async function expireById(sessionId) {
+  const expiredSessionObject = await runUpdateQuery(sessionId);
+  return expiredSessionObject;
+
+  async function runUpdateQuery(sessionId) {
+    const results = await database.query({
+      text: `
+        UPDATE
+          sessions
+        SET
+          expires_at = expires_at - interval '1 year',
+          updated_at = NOW()
+        WHERE
+          id = $1
+        RETURNING
+          *
+        ;`,
+      values: [sessionId],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
   findOneValidByToken,
   renew,
+  expireById,
   EXPIRATION_IN_MILLISECONDS,
 };
 
